26c99196d88e2b2204343f700637fa3bc6e7a889
Koden <yenkoded@gmail.com>
2026-06-08 13:20:59 +0100

    debug: add markers to both scholarship and requests views

--- views/pages/scholarships.php
+++ views/pages/scholarships.php
@@ -1,4 +1,5 @@
 <?php require APPROOT . '/views/layouts/header.php'; ?>
+<h1 style="color: red; background: yellow; text-align: center; padding: 20px;">DEBUG: SCHOLARSHIPS VIEW - VERSION 1</h1>
 
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
